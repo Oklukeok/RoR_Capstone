2f88f4484ec61c7aab1116ba2c022847f3478ba1
Oklukeok <lucasabermudez@gmail.com>
2023-02-24 17:03:46 -0600

    Functionalities + styles

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,31 +1,36 @@
 class EntitiesController < ApplicationController
-  before_action :set_entity, only: %i[ show edit update destroy ]
+  before_action :set_entity, only: %i[show edit update destroy]
 
   # GET /entities or /entities.json
   def index
-    @entities = Entity.all
+    @current_user = current_user
+    @entities = Entity.where(user_id: current_user.id)
+    @groups = Group.where(user_id: current_user.id)
   end
 
   # GET /entities/1 or /entities/1.json
-  def show
-  end
+  def show; end
 
   # GET /entities/new
   def new
+    @current_user = current_user
     @entity = Entity.new
+    @groups = Group.where(user_id: current_user.id)
+    @users = User.where(id: current_user.id)
   end
 
   # GET /entities/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /entities or /entities.json
   def create
+    @current_user = current_user
+    @users = User.where(id: current_user.id)
     @entity = Entity.new(entity_params)
 
     respond_to do |format|
       if @entity.save
-        format.html { redirect_to entity_url(@entity), notice: "Entity was successfully created." }
+        format.html { redirect_to entity_url(@entity), notice: 'Entity was successfully created.' }
         format.json { render :show, status: :created, location: @entity }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +43,7 @@ class EntitiesController < ApplicationController
   def update
     respond_to do |format|
       if @entity.update(entity_params)
-        format.html { redirect_to entity_url(@entity), notice: "Entity was successfully updated." }
+        format.html { redirect_to entity_url(@entity), notice: 'Entity was successfully updated.' }
         format.json { render :show, status: :ok, location: @entity }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +57,20 @@ class EntitiesController < ApplicationController
     @entity.destroy
 
     respond_to do |format|
-      format.html { redirect_to entities_url, notice: "Entity was successfully destroyed." }
+      format.html { redirect_to entities_url, notice: 'Entity was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_entity
-      @entity = Entity.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def entity_params
-      params.require(:entity).permit(:authorId, :name, :amount)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_entity
+    @entity = Entity.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def entity_params
+    params.require(:entity).permit(:user_id, :group_id, :name, :amount)
+  end
 end
--- db/migrate/20230220163405_create_entities.rb
+++ db/migrate/20230220163405_create_entities.rb
@@ -1,7 +1,8 @@
 class CreateEntities < ActiveRecord::Migration[7.0]
   def change
     create_table :entities do |t|
-      t.integer :authorId
+      t.references :user
+      t.references :group
       t.string :name
       t.integer :amount
 
